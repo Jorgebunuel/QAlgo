f13da14eecb6aff7687ae70b98d73059f37bea7e
Jorgebunuel <93011325+Jorgebunuel@users.noreply.github.com>
2021-11-14 20:10:46 +0100

    up

--- Porfolio/Pesos_SharpeRatio.R
+++ Porfolio/Pesos_SharpeRatio.R
@@ -10,46 +10,61 @@ library(plotly)
 library(alphavantager)
 library(quantmod)
 library(quantstrat)
-#install.packages("quantstrat")
 
-source(file = "D:/Users/jjbunuel/Desktop/TRADING_QUANTITATIVO/BIBLIOTECA/SQL_CALLS.R")
-source(file = "D:/Users/jjbunuel/Desktop/TRADING_QUANTITATIVO/BIBLIOTECA/Funciones_Datos_historicos.R")
-source(file = "D:/Users/jjbunuel/Desktop/TRADING_QUANTITATIVO/BIBLIOTECA/ESTRATEGIAS.R")
+#1V0 DIRECTORIO DE TRABAJO ##############################################################
+#masterPath<-"D:/DATOS_GESTAMP/jorge_datos/TRADING_QUANTITATIVO/01Dise?oSistema/"
+masterPath<-"./"
+#1V0 DIRECTORIO DE TRABAJO ##############################################################
+library(PerformanceAnalytics)
+library(tidyquant)
+source(file = paste0(masterPath,"Librerias/all.R"))
+source(file = paste0(masterPath,"BIBLIOTECA/ReadFunXTB.R"))
 
 ############################################################################################
 ## Analysis del Grafico
 ############################################################################################
-DATA_ALL<-read.csv(file = "D:/Users/jjbunuel/Desktop/TRADING_QUANTITATIVO/MSFT.txt")
-TD<-nrow(DATA_ALL)
-DTr<-round(TD*0.66,0)
-DTs<-TD-DTr
-
-DATA_ALL%>%
-  ggplot(aes(x=as.Date(DayTime),y=C))+geom_line()+geom_vline(xintercept = as.Date(DATA_ALL$DayTime[DTr]))
-
-
-############################################################################################
-## Llamada datos
-############################################################################################
+EURUSD<-LecturaXTBForex("EURUSD","1h")
+EURJPY<-LecturaXTBForex("EURJPY","1h")
 
 
 
 
 
-DATA_ALL<-QResults_DB_BT_1(818)
-summary(DATA_ALL)
-
-DATA_ALL%>%
-  mutate(BT=ifelse(RC>0,"G","P"))%>%
-  ggplot(aes(x=RC_Ts_n,fill=BT))+geom_histogram()
 
 
-DATA_ALL_new<-DATA_ALL%>%
-  group_by(nn)%>%
-  mutate(best_index=ifelse(max(RC)==RC, 1, 0))
 
-DATA_ALL_new<-DATA_ALL_new%>%
-  filter(best_index==1)
+
+
+
+
+
+
+
+
+serie1<-xts(EURUSD$Close,order.by=(as.Date(EURUSD$Datetime)))
+serie2<-xts(EURJPY$Close,order.by=(as.Date(EURJPY$Datetime)))
+
+diff(log((serie1)))[-1]
+comparison <- cbind(cumsum( diff(log((serie1)))[-1]), cumsum( diff(log((serie2)))[-1]))
+colnames(comparison)  <- c("s1", "s2")
+chart.TimeSeries(comparison, legend.loc = "topleft",colorset =c("green", "red"))
+charts.PerformanceSummary(cbind(( diff(log((serie1)))[-1]), ( diff(log((serie2)))[-1])))
+t(table.CalendarReturns(comparison))
+chart.Boxplot(comparison)
+chart.Correlation(comparison)
+chart.RiskReturnScatter(comparison, Rf = .04/12, add.boxplots = TRUE)
+
+
+layout(rbind(c(1,2),c(3,4)))
+chart.Histogram(comparison[,1,drop=F], main = "Plain", methods = NULL)
+chart.Histogram(comparison[,1,drop=F], main = "Density", breaks=40, methods = c("add.density", "add.normal"))
+chart.Histogram(comparison[,1,drop=F], main = "Skew and Kurt", methods = c("add.centered", "add.rug"))
+chart.Histogram(comparison[,1,drop=F], main = "Risk Measures", methods = c("add.risk"))
+
+
+table.CAPM(comparison,comparison, Rf = comparison)
+
+
 
 
 
